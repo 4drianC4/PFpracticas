1bc15a0cc1254016817698a2bdbb776f268f2e31
4drianC4 <camararequeadrianrafael@gmail.com>
2022-11-22 01:44:44 -0400

    pc commit

--- Practica6.hs
+++ Practica6.hs
@@ -21,24 +21,66 @@ mylength [] = 0
 mylength (x:xs) = (mylength xs) + 1
 
 vmat [] = []
-vmat (x:xs) = if ((mylength x) == mylength (head xs)) then True:vmat xs else [False]
+vmat (x:xs) = if(mylength xs > 0)then (((mylength x) == mylength (head xs)):vmat xs)
+                else True:vmat xs
+
+verificarmat xs = and (vmat xs) 
 
 --4. Definir una función que reciba 1 matriz y una función de orden y 
 --devuelva True si la matriz esta ordenada de acuerdo a la función de 
 --orden.
+
+--orden filas
+ordf [] c = True
+ordf [_] c = True
+ordf (x:y:xs) c = (c x y)&&(ordf (y:xs) c)
+
+--concat 
+myconcat [] = []
+myconcat (xs:xss) = (xs)++(myconcat xss)
+
+--orden matrices
+ordm (xss) c = (ordf (myconcat xss) c)
+
 --5. Definir una función que reciba una lista de números y devuelva todos
 --los números pares
+
+pars [] = []
+pars (x:xs) = if (mod x 2 == 0) then x:pars xs else pars xs 
+
 --6. Definir una función que reciba una lista de listas y devuelva solo 
 --aquellas cuya longitud sea par.
+
+longpars [] = []
+longpars (xs:xss) = if (f == 0) then xs:longpars xss else longpars xss 
+    where f = mod (mylength xs) 2
+
 --7. Definir una función que reciba una lista de listas de números y 
 --borre todos los números pares de estas listas
+
+nopars [] = []
+nopars (x:xs) = if (mod x 2 /= 0) then x:nopars xs else nopars xs 
+noparss [] = []
+--nopars [_] = []
+noparss (xs:xss) = if (mod x 2 /= 0) then ((nopars xs):noparss xss) else (noparss xss)
+    where x = head (xs) 
+
 --8. Definir una función que reciba una lista de listas y devuelva una 
 --lista formada por los penúltimos elementos de las listas
+
+
+
 --9. Definir una función que reciba un número y devuelva una lista con los
 --posibles divisores del número.
+
+
+
 --10. Definir una función que busque un elemento en una lista mediante 
 --búsqueda secuencial (la función debería devolver la posición donde se
 --encuentra el elemento).
+
+
+
 --11. Definir una función que busque un elemento en una lista mediante 
 --búsqueda binaria (la función debería devolver la posición donde está el
 --elemento).
